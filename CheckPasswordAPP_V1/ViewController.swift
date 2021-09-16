5953a8775f274d2ef0a79cedf1d83a1bfd2cb627
fred.fu <fjw08199999@gmail.com>
2021-09-16 17:19:48 +0800

    Initial Commit

---
+++ CheckPasswordAPP_V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckPasswordAPP_V1
+//
+//  Created by fred fu on 2021/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
