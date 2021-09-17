c9d08ec210f1d1cb2f7078b4df98e54356c0e746
fred.fu <fjw08199999@gmail.com>
2021-09-17 10:43:11 +0800

    1, 建立 File CheckPasswordData.swift 2. 建立 CheckPasswordViewController.swift 3. VIEW畫面完成

---
+++ CheckPasswordAPP_V1/CheckPasswordData.swift
@@ -0,0 +1,12 @@
+//
+//  CheckPasswordData.swift
+//  CheckPasswordAPP_V1
+//
+//  Created by fred fu on 2021/9/16.
+//
+
+import Foundation
+
+enum PasswordCheck {
+    
+}
---
+++ CheckPasswordAPP_V1/CheckPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CheckPasswordViewController.swift
+//  CheckPasswordAPP_V1
+//
+//  Created by fred fu on 2021/9/16.
+//
+
+import UIKit
+
+class CheckPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
